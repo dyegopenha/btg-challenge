76d6f115491f982977cb058c4d5bcc3752a34160
Dyego Penha <dyegopenha@hotmail.com>
2025-08-30 19:04:57 -0300

    init project

---
+++ src/test/java/com/example/btg_challenge/BtgChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btg_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
